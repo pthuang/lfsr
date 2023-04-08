3e67cdc3da4ce211a66d331e830a42004087cc2e
pthuang <weitao_cn@163.com>
2023-04-08 18:51:30 +0800

    update verilog module and add .m function for generating code snippet

--- lsfr_test/script/tb_galois_lsfr_do/tb_galois_lsfr_compile.do
+++ lsfr_test/script/tb_galois_lsfr_do/tb_galois_lsfr_compile.do
@@ -2,6 +2,7 @@
 vlog -93 -work work $SIM_HOME/tb_galois_lsfr/tb_galois_lsfr.v \
 $SRC_HOME/galois_lsfr/galois_lsfr.v
 
+
 vsim -t ns -voptargs=+acc work.tb_galois_lsfr 
 
 set NumericStdNoWarnings 1
---
+++ matlab/lsfr_tap_gen.m
@@ -0,0 +1,77 @@
+%% clear workspace
+clc; clear; close all; 
+
+%% get tap  
+lsfr_tap = TapInitial();
+
+
+%% galois tap gernerate 
+temp = zeros(size(lsfr_tap,1),1);
+galois_tap = [temp, lsfr_tap(:,1:end-1)];
+
+cnt_zero_len = 0;
+fid = fopen('galois_verilog_snippet.v','wt');
+fprintf(fid,'generate\n'); % generate
+fprintf(fid,'    case(BIT_WIDTH)\n'); 
+for i = 3:168 
+    fprintf(fid,'    ');
+    fprintf(fid, '%03d', i); 
+    fprintf(fid,'     : begin always@(*) begin ');
+    fprintf(fid, 'fb_vec = {');
+    for j = i:-1:1
+        if j == 1
+            if galois_tap(i,j) == 1
+                fprintf(fid, 'x_lsfr[BIT_WIDTH]}'); %
+            else 
+                fprintf(fid, '%d''h0}', cnt_zero_len+1); % 
+                cnt_zero_len = 0;
+            end
+        else
+            if galois_tap(i,j) == 1
+                if cnt_zero_len > 0
+                    fprintf(fid, '%d''h0,', cnt_zero_len); % 
+                    cnt_zero_len = 0;
+                end
+                fprintf(fid, 'x_lsfr[BIT_WIDTH],'); %
+                
+            else 
+                cnt_zero_len = cnt_zero_len + 1;
+            end
+        end
+    end 
+    fprintf(fid,'; end end \n');
+end
+fprintf(fid,'    default : begin always@(*) begin fb_vec = 0; end end\n');
+fprintf(fid,'    endcase\n'); 
+fprintf(fid,'endgenerate\n'); % generate
+
+
+%% fibonacci tap gernerate 
+fid = fopen('fibonacci_verilog_snippet.v','wt');
+fprintf(fid,'generate\n'); % generate
+fprintf(fid,'    case(BIT_WIDTH)\n'); 
+for i = 3:168 
+    fprintf(fid,'    ');
+    fprintf(fid, '%03d', i); %
+    fprintf(fid,'     : assign r_xnor = ');
+    for j = i:-1:1
+
+        if j == i
+            if lsfr_tap(i,j) == 1
+                fprintf(fid, 'r_lsfr[%03d]}', j); %
+            end
+        elseif j == 1
+            if lsfr_tap(i,j) == 1
+                fprintf(fid, ' ^~ r_lsfr[%03d]}', j); %
+            end
+        else
+            if lsfr_tap(i,j) == 1
+                fprintf(fid, ' ^~ r_lsfr[%03d]', j); %
+            end
+        end
+    end 
+    fprintf(fid,';\n');
+end
+fprintf(fid,'    endcase\n'); 
+fprintf(fid,'endgenerate\n'); % generate
+
